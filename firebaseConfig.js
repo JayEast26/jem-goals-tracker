29325b0a35ef600c817a70d080670f030bb58c9c
jayeast646@gmail.com <lloydtolero24@gmail.com>
2025-08-28 11:43:25 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdkGBxFN01zmiF02IFV86GILoaRW7Xcbw",
+  authDomain: "jem-goal-tracker.firebaseapp.com",
+  projectId: "jem-goal-tracker",
+  storageBucket: "jem-goal-tracker.firebasestorage.app",
+  messagingSenderId: "31585386651",
+  appId: "1:31585386651:web:ac280d9db2d18bdda6c63e"
+};
+
+const app = initializeApp(firebaseConfig);
