a56ea5b3b6e199a2e1bb424533a20ddcb7241e85
JayEast26 <lloydtolero24@gmail.com>
2025-09-02 09:44:01 +0800

    Firebase Configuration

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,12 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
-// Optionally import the services that you want to use
-// import {...} from 'firebase/auth';
-// import {...} from 'firebase/database';
-// import {...} from 'firebase/firestore';
-// import {...} from 'firebase/functions';
-// import {...} from 'firebase/storage';
-
-// Initialize Firebase
 const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+  apiKey: "AIzaSyBdkGBxFN01zmiF02IFV86GILoaRW7Xcbw",
+  authDomain: "jem-goal-tracker.firebaseapp.com",
+  projectId: "jem-goal-tracker",
+  storageBucket: "jem-goal-tracker.firebasestorage.app",
+  messagingSenderId: "31585386651",
+  appId: "1:31585386651:web:ac280d9db2d18bdda6c63e"
 };
 
 const app = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
