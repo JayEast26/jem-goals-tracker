2ea904021e387f8ca27930b44a190604523641dc
LloydTresTolero <lloydtolero24@gmail.com>
2025-09-04 10:03:53 +0800

    Data inserted to the firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBdkGBxFN01zmiF02IFV86GILoaRW7Xcbw",
